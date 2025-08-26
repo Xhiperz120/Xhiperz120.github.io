ea80512c05a712d5dae38ac1b99d3064d24dae31
Xhiperz120 <desindra0102@gmail.com>
2025-08-26 19:42:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+ function Fahri() {
+      alert("Refleksi Individu\n" +
+            "1. Bagian yang saya pahami yaitu HTML karena strukturnya mudah dipelajari\n" +
+            "2. Bagian yang masih sulit yaitu JavaScript terutama logika dan fungsi interaktif\n" +
+            "3. Fitur yang ingin ditambahkan yaitu kolom untuk menambahkan foto-foto lainnya");
+   }
+function Desindra() {
+  alert("Refleksi Individu\n" +
+"1. paling mudah html karena mudah untuk dipahami dan diterapkan\n" +
+"2. javascript karena harus memakai kode kode yg lumayan rumit\n" +
+"3. mungkin dibikin lebih wah dari segi tombol tombolnya");
+}
+
+
+    function Aldi() {
+      alert(
+        "1.Bagian mana yang paling kamu pahami (HTML, CSS, atau JS)?\n" +
+        "Saya paling memahami HTML, karena baru mulai belajar dan HTML terasa paling dasar dan mudah dimengerti. Saya bisa membuat struktur halaman seperti paragraf, gambar, dan link.\n\n" +
+
+        "2.Bagian mana yang masih sulit?\n" +
+        "Bagian yang masih susah buat saya adalah JavaScript, karena banyak konsep yang masih baru, seperti fungsi, event, atau bagaimana cara membuat elemen di web bisa interaktif. Saya masih butuh waktu untuk memahami cara kerjanya.\n\n" +
+
+        "3.Fitur apa yang ingin kamu tambahkan kalau ada waktu lebih?\n" +
+        "Kalau punya waktu lebih, saya ingin menambahkan fitur navigasi yang lebih interaktif, misalnya tombol scroll ke atas atau menu yang bisa dibuka-tutup, dan mungkin juga form sederhana yang bisa dipakai pengunjung atau teman untuk mengirim pesan."
+      );
+    }
